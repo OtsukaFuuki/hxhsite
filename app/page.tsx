7ecdcf97c7fd0bcf3e2c9582252d331c2ac66b45
OtsukaFuuki <ys.training231129@gmail.com>
2024-12-21 16:46:07 +0900

    refactor : sp時に余白が付くように修正

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 sm:px-0 px-6">
       <h1 className="text-3xl font-bold mb-4">HxHクイズアプリ</h1>
       {showQuiz ? <Quiz onRestart={handleRestart} /> : null}
     </main>
