d1e201726d8e6fafba34a982c5a860a2bef5c032
OtsukaFuuki <ys.training231129@gmail.com>
2024-12-29 15:40:38 +0900

    feat : ドロップダウンリストの作成

---
+++ app/feature/Components/Dropdown.tsx
@@ -0,0 +1,38 @@
+// Dropdown.tsx
+import React from "react";
+
+type DropdownProps = {
+  options: string[]; // ドロップダウンの選択肢
+  selectedValue: string;
+  onChange: (value: string) => void; // 値が変更されたときの処理
+  label?: string; // ドロップダウンのラベル
+};
+
+const Dropdown: React.FC<DropdownProps> = ({
+  options,
+  selectedValue,
+  onChange,
+  label,
+}) => {
+  return (
+    <div className="flex flex-col items-center">
+      <label className="text-white text-lg">{label}</label>
+      <select
+        value={selectedValue}
+        onChange={(e) => onChange(e.target.value)}
+        className="bg-gray-200 text-white border border-gray-300 rounded-md p-2 w-40 text-sm bg-gradient-to-r from-black via-indigo-700 to-purple-800 shadow-md hover:bg-indigo-800 focus:outline-none "
+      >
+        <option value="" disabled>
+          選択してください
+        </option>
+        {options.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Dropdown;
--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,26 @@ import Quiz from "./feature/Components/Quiz";
 
 import AudioPlayerWithVolume from "./feature/Components/AudioPlayer";
 import DifficultySelector from "./feature/Components/DifficultySelector";
+import Dropdown from "./feature/Components/Dropdown";
 
 export default function Home() {
   const [difficulty, setDifficulty] = useState<
     "beginner" | "intermediate" | "advanced" | null
   >(null);
   const [backgroundClass, setBackgroundClass] = useState("");
+  const [selectedDifficulty, setSelectedDifficulty] = useState("");
+  const handleDifficultyChange = (value: string) => {
+    setSelectedDifficulty(value);
+  };
+  const seriesOptions = [
+    "ハンター試験編",
+    "ククルーマウンテン編（キルアの実家）",
+    "天空闘技場編",
+    "ヨークシンシティ編",
+    "グリードアイランド編",
+    "キメラアントNGL編",
+    "キメラアント王誕生編",
+  ];
 
   const backgroundClasses = [
     "background-image2",
@@ -33,8 +47,8 @@ export default function Home() {
 
   return (
     <div className={`${backgroundClass} fsm:px-4 px-2`}>
-      <header>
-        <h1 className="text-2xl font-bold sm:mb-4 mb-0 sm:p-4 p-1 flex justify-start">
+      <header className="flex items-start min-h-[52px] sm:mb-4 mb-0 sm:p-4 p-1">
+        <h1 className="text-2xl font-bold  ">
           <Image
             src="/images/header/logo1.png"
             alt="HxH ロゴ"
@@ -42,14 +56,21 @@ export default function Home() {
             height={100}
             style={{ marginRight: "8px" }}
           />
-          <AudioPlayerWithVolume />
-          <button
-            onClick={() => location.reload()}
-            className="ml-2 px-4 py-2 text-sm bg-customGreen text-white rounded hover:bg-customGreen-dark"
-          >
-            Topへ
-          </button>
         </h1>
+        <AudioPlayerWithVolume />
+        <button
+          onClick={() => location.reload()}
+          className="ml-2 px-3 py-2 text-sm bg-customGreen text-white rounded hover:bg-customGreen-dark"
+        >
+          Topへ
+        </button>
+        <div className="ml-2">
+          <Dropdown
+            options={seriesOptions}
+            selectedValue={selectedDifficulty}
+            onChange={handleDifficultyChange}
+          />
+        </div>
       </header>
       <div className="flex flex-col items-center justify-center">
         <div className="flex flex-col sm:flex-row items-center justify-center sm:space-x-6 space-y-6 sm:space-y-0 w-full">
