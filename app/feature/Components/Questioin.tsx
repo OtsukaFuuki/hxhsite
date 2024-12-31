702c63e0cec7a9dc00cf7968ab5d0ddd8a400047
OtsukaFuuki <ys.training231129@gmail.com>
2024-12-31 11:17:36 +0900

    feat : 問題の修正1231117

--- app/feature/Components/Questioin.tsx
+++ app/feature/Components/Questioin.tsx
@@ -21,7 +21,7 @@ export const Question = ({ quiz, onAnswer }: QuestionProps) => {
   return (
     <div className="p-4 border border-gray-300 rounded shadow-lg max-w-lg w-full">
       {/* 問題部分 */}
-      <h2 className="text-white text-sm font-semibold mb-4 text-center min-h-[80px] flex items-center justify-center">
+      <h2 className="text-white text-sm text-left font-semibold mb-4  min-h-[80px] flex items-center justify-center">
         {quiz.question}
       </h2>
       {/* 回答フォーム */}
